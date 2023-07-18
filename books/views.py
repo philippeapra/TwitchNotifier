2b916ab6ad166bd26cb321d48164b7815508623d
Your Name <you@example.com>
2023-07-18 23:52:53 +0300

    45

--- books/views.py
+++ books/views.py
@@ -1,4 +1,3 @@
-from flask import  jsonify
 from books.twitch import send_twitch_request
 from django.views.generic import ListView
 from .models import Book
@@ -12,23 +11,14 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 @csrf_exempt
 def eventsub_callback(request):
-    print ("zzzzzzzzzzzzzzrequest msg:")
-    #print(request.GET)
-    
     if request.method == 'POST':
         print(request.POST)
         # Process the EventSub notification here
         # Access the payload using request.body or request.POST
-        # if challenge !=None:
-        #     return jsonify({"challenge": challenge}), 200
-            #send_twitch_request('eventsub/',{'challenge':challenge},None,"POST",None) 
-        #instance.subtitle = request.POST.get('created_at')
-        #instance.author = request.POST.get('status')
         instance = Book.objects.first()
         if request.headers.get('Twitch-Eventsub-Message-Type') == 'notification':
             instance.title='streamer online'
         payload= json.loads(request.body)
-        #instance.title=str(payload)
         try:
             challenge = payload['challenge']
         except:
@@ -38,14 +28,11 @@ def eventsub_callback(request):
             instance.title=""
         instance.save()
         if request.headers.get('Twitch-Eventsub-Message-Type') == 'webhook_callback_verification':
-            #return jsonify({"challenge": challenge}), 200
             return HttpResponse(status=200,content=challenge)
         return HttpResponseRedirect('/books/book_list.html',status=200)
     else:
         print(request.GET)
         return HttpResponse(status=405)
 
-
-
 # Client_ID: 35a40a7mket0skoaja7p70i205qf8z
-# Client_secret: d89jvmhf6ehs8lyt9qsbgx8exddc02
+# Client_secret: 
